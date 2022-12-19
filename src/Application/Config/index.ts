ae872ae7f34695005eca6c454a547ed3f30edd78
moeenn <moeen.v8@gmail.com>
2022-12-19 12:06:34 +0500

    removed deprecated DatabaseConfig class

--- src/Application/Config/index.ts
+++ src/Application/Config/index.ts
@@ -1,4 +1,3 @@
 export { EnvironmentConfig } from "./EnvironmentConfig"
 export { ServerConfig } from "./ServerConfig"
 export { AuthConfig } from "./AuthConfig"
-export { DatabaseConfig } from "./DatabaseConfig"
