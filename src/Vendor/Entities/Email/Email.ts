e934f420d4e2b66c55a20ab714780ab09526cb7c
moeenn <moeen.v8@gmail.com>
2022-12-19 17:00:22 +0500

    email template rendering (in-progress)

---
+++ src/Vendor/Entities/Email/Email.ts
@@ -0,0 +1,15 @@
+import { marked } from "marked"
+
+/**
+ *  all emails that this system sends must implement this abstract class
+ *
+ */
+export abstract class Email {
+  abstract subject: string
+  abstract template(): string
+
+  html(): string {
+    const md = this.template()
+    return marked.parse(md)
+  }
+}
